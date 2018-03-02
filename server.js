97d7006ebd1df1176f256eedf10c2ea074855911
oreonx <30339313+oreonx@users.noreply.github.com>
2018-03-02 19:05:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -181,7 +181,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/main.js', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+    res.sendFile(path.join(__dirname, 'ui', 'main.kjs'));
 });
 
 app.get('/ui/madi.png', function (req, res) {

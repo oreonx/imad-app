00577a261676a387606850d90c7c12291964f15e
oreonx <30339313+oreonx@users.noreply.github.com>
2018-03-02 19:36:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,13 @@ element.innerHTML = "New Value";
 
 //Move the image (Madi)
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveright() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
+    var interval = setInterval(moveright, 100); //Every 100ms, apply this moveright function
     img.style.marginLeft = '100px';
 }; 
     

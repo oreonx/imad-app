89d655e034c6d493b13930a4221788536f8794e1
oreonx <30339313+oreonx@users.noreply.github.com>
2018-03-07 01:50:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/test-db', function (req, res) {
         else {
             res.send(JSON.stringify(result.rows));
         }
-});
+    });
 });
 
 var app = express();
@@ -189,25 +189,6 @@ function createTemplate (data) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/articles/:articleName', function (req, res) {
-    //articleName == article-one
-
-  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result) {
-     if (err) {
-         res.status(500).send(err.toString());
-    } 
-    else {
-      if (results.rows.length === 0) {
-          res.status(404).send('Article not found!');
-      } 
-      else {
-          var articleData = result.rows[0];
-          res.send(createTemplate(articleData));
-      }        
-    } 
-  });
 });
 
 app.get('/ui/style.css', function (req, res) {

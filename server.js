fec1dd9f9f2cd372df1379c5935759bcf236a38d
oreonx <30339313+oreonx@users.noreply.github.com>
2018-03-07 01:45:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ var config = {
     
 
 var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+    pool.query('SELECT * FROM test', function(err, result) {
+        if(err) {
+            res.status(500).send(err.toString());
+        }
+        else {
+            res.send(JSON.stringify(result));
+        }
+});
+});
 
 var app = express();
 app.use(morgan('combined'));

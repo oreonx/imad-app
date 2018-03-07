0ad8459054a42989643f0b3ecdb4aaabcbe90f47
oreonx <30339313+oreonx@users.noreply.github.com>
2018-03-07 21:02:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -183,7 +183,7 @@ var Pool = Pool(config);
 app.get('/articles/:articleName', function (req, res) {
     var articleName = req.params.articleName;
  
-  pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err,result) {
+  Pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err,result) {
        if(err)
        {
            res.status(500).send(err.toString());

c13563ed5897336691c85aacb044869a7ac10dcd
oreonx <30339313+oreonx@users.noreply.github.com>
2018-03-07 20:50:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -197,7 +197,7 @@ app.get('/articles/:articleName', function (req, res) {
            else
            {
                var articleData=result.rows[0];
-               res.send(templates(articleData));
+               res.send(createTeemplate(articleData));
            }
        }
   });

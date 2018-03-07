3306260ed47cbf49e6cd02ba9be527d7bbe5d45f
oreonx <30339313+oreonx@users.noreply.github.com>
2018-03-07 20:43:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -186,7 +186,7 @@ app.get('/articles/:articleName', function (req, res) {
   pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName+"'",function(err,result) {
        if(err)
        {
-           res.status(500).send(err.toString());
+           res.status(550).send(err.toString());
        }
        else
        {
